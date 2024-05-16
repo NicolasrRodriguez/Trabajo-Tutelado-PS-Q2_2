7dbf638f6484207be4c6e6b9fc2a8bff3788a9da
NicolasrRodriguez <nicolas.rodriguez2@udc.es>
2024-05-16 18:54:45 +0200

    se puede seleccionar una imagen

--- app/src/main/java/com/example/triptracks/Presenter/ImagesActivityVIew.java
+++ app/src/main/java/com/example/triptracks/Presenter/ImagesActivityVIew.java
@@ -1,9 +1,16 @@
 package com.example.triptracks.Presenter;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresExtension;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
 import com.example.triptracks.Datos.FirebaseAuthData;
@@ -16,6 +23,8 @@ public class ImagesActivityVIew extends AppCompatActivity implements View.OnClic
 
     private String UserEmail;
 
+    private String imageSelected;
+
     private FirebaseAuthData firebaseAuth = new FirebaseAuthData();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +33,33 @@ public class ImagesActivityVIew extends AppCompatActivity implements View.OnClic
         setContentView(binding.getRoot());
         UserEmail = firebaseAuth.email();
         binding.botonVolver.setOnClickListener(this);
+        binding.escogerimagen.setOnClickListener(this);
+
 
 
     }
 
+    ActivityResultLauncher<Intent> myStartActivityForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                assert result.getData() != null;
+                Uri iamgeuri = result.getData().getData();
+                imageSelected = iamgeuri.toString();
+                binding.image.setImageURI(iamgeuri);
+            }
+    );
+
+    @RequiresExtension(extension = Build.VERSION_CODES.R, version = 2)
     @Override
     public void onClick(View v) {
         if(v == binding.botonVolver){
-            Intent resultIntent = new Intent();;
+            Intent resultIntent = new Intent();
             this.setResult(ItineraryDetailActivity.RESULT_OK, resultIntent);
             this.finish();
+        }
+        else if (v == binding.escogerimagen){
+            Intent resultImageIntent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+            myStartActivityForResult.launch(resultImageIntent);
         }
     }
 }
