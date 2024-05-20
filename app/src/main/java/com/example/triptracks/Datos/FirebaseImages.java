b561bcc77cb72b020c838bdac96584391e9c2d26
NicolasrRodriguez <nicolas.rodriguez2@udc.es>
2024-05-20 17:54:04 +0200

    Imagenes a storage-2.1

--- app/src/main/java/com/example/triptracks/Datos/FirebaseImages.java
+++ app/src/main/java/com/example/triptracks/Datos/FirebaseImages.java
@@ -12,8 +12,11 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 public class FirebaseImages {
 
@@ -48,6 +51,23 @@ public class FirebaseImages {
             onFailure.accept("File URI or user is null");
         }
 
+    }
+
+    private void saveDocumentInfo(String documentId, String documentName, String documentDescription, String downloadUrl, Consumer<String> onSuccess, Consumer<String> onFailure) {
+        if (user != null) {
+            long timestamp = System.currentTimeMillis();
+            Document document = new Document(documentId, documentName, documentDescription, downloadUrl, timestamp);
+            databaseReference.child(documentId).setValue(document)
+                    .addOnSuccessListener(aVoid -> {
+                        onSuccess.accept("Document added successfully");
+                        Map<String, Object> updates = new HashMap<>();
+                        updates.put("documentId", documentId);
+                        databaseReference.child(documentId).updateChildren(updates);
+                    })
+                    .addOnFailureListener(e -> onFailure.accept("Failed to add document: " + e.getMessage()));
+        } else {
+            onFailure.accept("User not logged in");
+        }
     }
 
 }
